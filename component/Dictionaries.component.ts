e54263e51a8001a18b9790da2974e4e96a5ffb8f
krzak <lonekwolf@gmail.com>
2023-07-23 18:22:07 +0200

    Refactoring

--- component/Dictionaries.component.ts
+++ component/Dictionaries.component.ts
@@ -1,8 +1,5 @@
 export class DictionariesComponent {
-    constructor(private page) {
-        const dictionaries = this.page.getByText('DDictionaries Tests');
-        dictionaries.click();
-    }
+    constructor(private page) { }
     filteredDictionaries = this.page.locator('a').filter({ hasText: 'Filtered Dictionaries' });
     staticDictionaries = this.page.locator('a').filter({ hasText: 'Static Dictionaries' });
     tableDictionaries = this.page.locator('a').filter({ hasText: 'Table Dictionaries' });
